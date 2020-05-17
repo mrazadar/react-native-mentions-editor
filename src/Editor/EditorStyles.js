d3a1406cae05194665f3bc1eff8e537fef11951c
ofer morag <ofer.morag@hibob.io>
2020-05-17 18:56:31 +0300

    Fixed: writing in RTL causes text to appear twice
    In addition, added the option to show mentions list on the bottom of the editor removed doubled CSS property
    (appologize for mixing these)

--- src/Editor/EditorStyles.js
+++ src/Editor/EditorStyles.js
@@ -21,9 +21,9 @@ export default StyleSheet.create({
     minHeight: 40,
     position: "absolute",
     top: 0,
-    color: "transparent",
     alignSelf: "stretch",
-    width: "100%"
+    width: "100%",
+    height: '100%' // added for android
   },
   formmatedTextWrapper: {
     minHeight: 40,
