6d23a267943c0562e741a5a7e4158cbc95a7c0d9
ofer morag <ofer.morag@hibob.io>
2023-01-23 23:16:53 +0200

    fix mention editor height issue

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -526,7 +526,7 @@ export class Editor extends React.Component {
           ? evt.nativeEvent.contentSize.height
           : evt.nativeEvent.contentSize.height - androidTextHeight;
       let editorHeight = 20;
-      editorHeight = Math.max(this.state.editorHeight, editorHeight + height) - 4;
+      editorHeight = editorHeight + height;
       this.setState({
         editorHeight
       });
